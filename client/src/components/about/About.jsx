4a0be2ca59f76da1db3f6aa0fc3407d436b47e35
Charixel <168551172+charitha-reddy1074@users.noreply.github.com>
2025-01-27 09:54:02 +0530

    Add files via upload

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,50 @@
+
+import { Box, styled, Typography, Link } from '@mui/material';
+import { GitHub, Instagram, Email } from '@mui/icons-material';
+import backgroundImage  from './bg2.png';
+
+const Banner = styled(Box)`
+    background-image: url(${backgroundImage});
+    width: 100%;
+    height: 50vh;
+    background-position: left 0px bottom 0px;
+    background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+    padding: 20px;
+    & > h3, & > h5 {
+        margin-top: 50px;
+    }
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+`;
+
+const About = () => {
+
+    return (
+        <Box>
+            <Banner/>
+            <Wrapper>
+                <Typography variant="h3">Code for Interview</Typography>
+                <Text variant="h5">I'm a IT Engineer based in India. 
+                    I've built websites, desktop applications and corporate software.<br />
+                    If you are interested, you can view some of my favorite projects here
+                    <Box component="span" style={{ marginLeft: 5 }}>
+                        <Link href="https://github.com/Hemanshi2954" color="inherit" target="_blank"><GitHub /></Link>
+                    </Box>
+                </Text>
+                <Text variant="h5">
+                    Need something built or simply want to have chat? Reach out to me by Email 
+                        <Link href="mailto:hemanship00@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
+                            <Email />
+                        </Link>.
+                </Text>
+            </Wrapper>
+        </Box>
+    )
+}
+
+export default About;
---
+++ database/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const Connection= async (username,password) =>{
+    const URL= `mongodb+srv://${username}:${password}@blog-website-db.rlxucag.mongodb.net/?retryWrites=true&w=majority&appName=Blog-Website-db`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+export default Connection;
